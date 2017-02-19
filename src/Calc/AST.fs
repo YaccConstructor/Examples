6b47debdd2347cbf086c58dc125a230b49e8fd0e
antkhorin <antkhorin@gmail.com>
2017-02-20 00:28:57 +0300

    Done

--- src/Calc/AST.fs
+++ src/Calc/AST.fs
@@ -14,10 +14,15 @@ type Op =
     | Mult
     | Div
     | Minus
+    | Sin
+    | Cos
+    | Abs
+    | Negate
 
 type Expr =
     | Num of float
     | EVar of Var
+    | UnaryOp of Op*Expr*float
     | BinOp of Op*Expr*Expr*float
 
 type Stmt = 
@@ -28,8 +33,18 @@ type result = list<Stmt>*Dictionary<string,float>*float //Statements, variables,
 
 let getValue e = match e with
    | Num num -> num
+   | UnaryOp (op,e,result) -> result
    | BinOp (op,l,r,result) -> result
    | EVar (name) -> vars.[name]
+
+
+let calcUnary op e = 
+    let operator = match op with
+    | Sin -> (sin)
+    | Cos -> (cos)
+    | Abs -> (abs)
+    | Negate -> (-) 0.0
+    UnaryOp(op,e,operator (getValue e))
 
 let calcFunc l (op, r) =
    let operator = match op with
